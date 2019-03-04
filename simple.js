f02793f04d0ce8b89faa861d79fb0fe0c57347f0
mollow5 <mollow5@naver.com>
2019-03-04 10:53:00 +0900

    Update simple.js

--- simple.js
+++ simple.js
@@ -20,8 +20,8 @@ console.log(paragraphs);
 const para1 = document.getElementsByTagName('p')[0];
 console.log(para1.textContent);
 console.log(para1.innerHTML);
-//para1.textContent = "Modified HTML file";
-para1.innerHTML = "Modified <i>HTML</i> file";
+//para1.textContent = "Modified HTML file"; // 태그를 반영하지 않는 형태 textContent
+para1.innerHTML = "Modified <i>HTML</i> file"; // 태그를 반영하는 형태 innerHTML
 
 const p1 = document.createElement('p');
 const p2 = document.createElement('p');
@@ -33,3 +33,18 @@ const firstChild = parent.childNodes[0];
 parent.insertBefore(p1,firstChild);
 parent.appendChild(p2);
 console.log(document.getElementById('content'));
+
+function highligthParas ( containing ){
+  if(typeof containing == 'string'){
+    containing = new RegExp(`\\b${containing}\\b`,'i'); // RegExp 정규표현식을 사용하기 위한 객체
+  }
+  const paras = document.getElementsByTagName('p');
+  console.log(paras);
+  for(let p of paras){
+    if ( !containing.test(p.textContent)){
+      continue;
+    }
+    p.classList.add('highlight');
+  }
+}
+highligthparas('unique');
